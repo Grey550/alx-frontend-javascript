369c9bda06e4e62a538c1840987f066582f1aa72
Angie <angie.monnye@gmail.com>
2023-11-27 20:03:08 +0200

    3.Parameter defaults

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,12 +1,3 @@
-export default function getSumOfHoods(initialNumber = 1, expansion1989, expansion2019) {
-  if (expansion1989 === undefined) {
-    // eslint-disable-next-line no-param-reassign
-    expansion1989 = 89;
-  }
-
-  if (expansion2019 === undefined) {
-    // eslint-disable-next-line no-param-reassign
-    expansion2019 = 19;
-  }
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
   return initialNumber + expansion1989 + expansion2019;
 }
