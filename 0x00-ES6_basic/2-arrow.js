b19ff4ba059dcafb26060b116662b8e9049a660c
Angie <angie.monnye@gmail.com>
2023-11-27 19:27:19 +0200

    2.Arrow functions

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,5 +1,5 @@
 export default function getNeighborhoodsList() {
-  this.sanFranciscoNeighborhoods = ['SONA', 'Union Square'];
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
   const self = this;
   this.addNeighborhood = (newNeighborhood) => {
--- 0x00-ES6_basic/2-main.js
+++ 0x00-ES6_basic/2-main.js
@@ -1,6 +1,5 @@
 import getNeighborhoodsList from './2-arrow.js';
 
-// eslint-disable-next-line new-cap
 const neighborhoodsList = new getNeighborhoodsList();
-// eslint-disable-next-line no-unused-vars
 const res = neighborhoodsList.addNeighborhood('Noe Valley');
+console.log(res);
