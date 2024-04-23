1d70690424a7b22f5f96073509cf5c67253c19dc
Angie <angie.monnye@gmail.com>
2024-04-23 02:01:15 +0200

    Iterate through object

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -2,9 +2,5 @@
 // the function's parameter is the return value from the previous function
 
 export default function iterateThroughObject(reportWithIterator) {
-  const result = [];
-  for (const item of reportWithIterator) {
-    result.push(item);
-  }
-  return result;
+  return reportWithIterator.join(' | ');
 }
