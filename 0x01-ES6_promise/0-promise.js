7e5e63e37ce34e2e61025c9e109b5d78a0fb19b3
Angie <angie.monnye@gmail.com>
2024-04-24 22:54:05 +0200

    Keep every promise you make and only make promises you can keep

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,11 @@
+function getResponseFromAPI(success = true) {
+    return new Promise((resolve, reject) => {
+      if (success) {
+        resolve('success');
+      } else {
+        reject(Error('error'));
+      }
+    });
+  }
+  
+  export default getResponseFromAPI;
